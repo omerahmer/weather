19ceb30e138b16ec7e799d6a7e5d1d318da2c946
omerahmer <omerahmer918@gmail.com>
2025-01-03 00:44:41 -0800

    clerk theme won't work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,14 @@
 import '@/styles/globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
-import { dark, neobrutalism } from '@clerk/themes'
+import { neobrutalism } from '@clerk/themes'
 import type { AppProps } from 'next/app'
-import { ProtectedRoute } from './protectedRoute'
 
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: dark,
-        signIn: { baseTheme: neobrutalism },
+        baseTheme: neobrutalism,
       }}
     >
       <Component {...pageProps} />
